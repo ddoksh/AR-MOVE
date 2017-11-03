bf81f895c90ece965972faae61104bcf7251a76f
ddoksh <33309281+ddoksh@users.noreply.github.com>
2017-11-03 10:27:49 +0900

    Initial commit

---
+++ Assets/ARAgent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ARAgent : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray camToTouchPos = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(camToTouchPos, out hit, 50f))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
---
+++ Assets/DragObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragObject : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private float distanceBetCam = 0;
+
+    public void OnDrag(PointerEventData data)
+    {
+        Vector2 touchPosition = data.position;
+
+        Vector3 touchPositionInWorld = Camera.main.ScreenToWorldPoint(new Vector3(
+            touchPosition.x,
+            touchPosition.y,
+            distanceBetCam)
+            );
+
+        transform.position = touchPositionInWorld;
+    }
+
+    public void OnBeginDrag(PointerEventData data)
+    {
+        Debug.Log("Drag Begin!");
+        distanceBetCam = Vector3.Distance(transform.position, Camera.main.transform.position);
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+    }
+}
